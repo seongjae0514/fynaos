fc59719430b312f5ad191ac8bb5a648d98cfcfd2
Seongjae Kim <kimseongjae0514@gmail.com>
2026-07-14 16:51:51 +0900

    Delete unused variables

--- kd/kd.c
+++ kd/kd.c
@@ -1,12 +1,6 @@
 #include <fynaos/kernel.h>
 #include <fynaos/kd.h>
 
-#define CALL_STACK_SIZE 62
-
-const char *call_stack[CALL_STACK_SIZE] = { NULL };
-int         top                         = -1;
-boolean_t   call_stack_overflowed       = FALSE;
-
 void kd_assert(boolean_t cond, const char *exp,
                const char *msg,
                const char *file, int line, const char *function)
